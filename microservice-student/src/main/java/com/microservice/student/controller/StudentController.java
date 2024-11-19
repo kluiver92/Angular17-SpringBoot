be43e7f37a218ef2b423379583a355bda3dcb7c8
antoniosanchezm <antoniojsanchez92@gmail.com>
2024-11-19 11:30:29 +0100

    update java21

--- microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -2,11 +2,15 @@ package com.microservice.student.controller;
 
 import com.microservice.student.entities.Student;
 import com.microservice.student.service.IStudentService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
@@ -22,6 +26,7 @@ public class StudentController {
 
     @GetMapping("/all")
     public ResponseEntity<?> findById(){
+    	log.info("Mostrando todos los estudiantes");
         return ResponseEntity.ok(studentService.findAll());
     }
 
